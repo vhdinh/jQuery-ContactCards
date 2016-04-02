d39dfad1822fe8ff0fa238591af40c65dc99755b
Vu Dinh <isthis.vu@gmail.com>
2016-04-02 14:48:58 -0700

    added fields validation to js

--- ContactCard.js
+++ ContactCard.js
@@ -9,12 +9,19 @@ $(document).ready(function(){
 		 first = $('#first').val();
 		 last = $('#last').val();
 		 description = $('#description').val();
-
+		 // make sure all fields are filled out
+		 if($('#first').val().length < 1 || $('#last').val().length < 1 || $('#description').val().length < 1 ){
+		 	alert("All fields must be filled out")
+		 	return false
+		 }
+		 else {
 		$('#right').append("<div reverse='" + description + "' class='ncontact'>" + first + " " + last + "<br> <span>Click for description!</span></div>")
 		
 		// clear form inputs when submitted
 		$('form').find("input:text").val("");
 		$('form').find("#description").val("");
+
+		}
 
 	$(document).on('click','.ncontact', function(){
 		console.log($(this).attr("reverse"));
